c79ae97e6a7961046ebcc70d16ce4c1b0079ea23
VoNgocToi <122598949+VoNgocToi@users.noreply.github.com>
2024-11-07 10:28:40 +0700

    new
    new

---
+++ src/main/java/com/hamter/service/BookingService.java
@@ -0,0 +1,27 @@
+package com.hamter.service;
+
+import java.util.List;
+
+import com.hamter.model.Booking;
+
+public interface BookingService {
+	
+	List<Booking> findAll();
+	
+	Booking findById(Long id);
+	
+	Booking create(Booking booking);
+	
+	Booking update(Booking booking);
+	
+	void delete(Long id);
+	
+	Booking confirmBooking(Long id);
+	
+	Booking cancelBooking(Long id);
+	
+	void sendReminders();
+	
+	Booking cancelBookingPending(Long id);
+
+}
