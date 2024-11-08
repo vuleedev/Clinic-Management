7d13ac8ef840a943a92bda09565d995430820ff4
VoNgocToi <122598949+VoNgocToi@users.noreply.github.com>
2024-11-08 09:47:04 +0700

    Update Bookings.java

--- src/main/java/com/hamter/model/Bookings.java
+++ src/main/java/com/hamter/model/Bookings.java
@@ -1,31 +1,30 @@
 package com.hamter.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import javax.persistence.*;
+
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @Entity
 @Table(name = "bookings")
-public class Bookings {
+public class Booking implements Serializable {
 
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String statusId;
+    
+    private String status2Id;
 
     private Integer doctorId;
 
     private String patientId;
+    
+    private String email;
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date date;
@@ -39,4 +38,5 @@ public class Bookings {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updatedAt", nullable = false)
     private Date updatedAt;
+
 }
