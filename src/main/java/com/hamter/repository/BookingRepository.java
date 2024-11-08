5fb944f1f4a053db8ba3b8c811043d66612133cb
VoNgocToi <122598949+VoNgocToi@users.noreply.github.com>
2024-11-08 09:50:55 +0700

    Update BookingRepository.java

--- src/main/java/com/hamter/repository/BookingRepository.java
+++ src/main/java/com/hamter/repository/BookingRepository.java
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-package com.hamter.repository;
-
-public interface BookingRepository {
-
-}
-=======
 package com.hamter.repository;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -18,9 +12,8 @@ import com.hamter.model.Booking;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 	
-	//List<Booking> findByStatusId(String statusId);
-	
 	@Query("SELECT b FROM Booking b WHERE b.date BETWEEN :start AND :end AND b.statusId = 'CONFIRMED'")
     List<Booking> findBookingsBetweenDates(Date start, Date end);
+	
+    Optional<Booking> findTopByPatientIdOrderByDateDesc(String patientId);
 }
->>>>>>> 545840a2952baa7f9c05c319d3d612e18e74e73a
