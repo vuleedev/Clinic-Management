d404e90efe98447db0223d26c4f7f8f8baba2030
ChienNGqt <chiennhpd09284@fpt.edu.vn>
2024-11-13 19:03:54 +0700

    Update ClinicService.java

--- src/main/java/com/hamter/service/ClinicService.java
+++ src/main/java/com/hamter/service/ClinicService.java
@@ -1,5 +1,36 @@
 package com.hamter.service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hamter.model.Clinic;
+import com.hamter.repository.ClinicRepository;
+
+@Service
 public class ClinicService {
+  
+@Autowired
+	private ClinicRepository clinicRepository;
+	
+	public List<Clinic> findAll() {
+		return clinicRepository.findAll();
+	}
 
+	public Clinic findById(Long id) {
+		return clinicRepository.findById(id).orElse(null);
+	}
+
+	public Clinic create(Clinic clinic) {
+		return clinicRepository.save(clinic);
+	}
+
+	public Clinic update(Clinic clinic) {
+		return clinicRepository.save(clinic);
+	}
+
+	public void delete(Long id) {
+		clinicRepository.deleteById(id);
+		
+	}
 }
