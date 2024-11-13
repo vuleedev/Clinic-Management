bc93a0b2d0d147da1f18b1e953ce41662b70328d
VoNgocToi <122598949+VoNgocToi@users.noreply.github.com>
2024-11-13 22:36:34 +0700

    Update EmailService bỏ Impl.java

--- src/main/java/com/hamter/service/EmailService.java
+++ src/main/java/com/hamter/service/EmailService.java
@@ -1,6 +1,23 @@
 package com.hamter.service;
 
-public interface EmailService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
 	
-	public void SendMailBooking(String toEmail, String subject, String body);
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	public void SendMailBooking(String toEmail, String subject, String body) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(toEmail);
+		message.setSubject(subject);
+		message.setText(body);
+		javaMailSender.send(message);
+		System.out.println("Email đã gửi đến: " + toEmail);
+	}
+	
 }
