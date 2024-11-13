f4ec9abaa1602e67ad6bf9aaaf5780b33dab5ef2
VoNgocToi <122598949+VoNgocToi@users.noreply.github.com>
2024-11-13 22:44:12 +0700

    model TimeSlot
    đại diện cho ca làm việc với IsAvailable = true -> ca rảnh, false -> ca bận

---
+++ TimeSlot.java
@@ -0,0 +1,43 @@
+package com.hamter.model;
+
+import javax.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "TimeSlots")
+public class TimeSlot implements Serializable {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "schedule_id", nullable = false)
+    private Schedule schedule;
+
+    @Temporal(TemporalType.TIME)
+    @Column(name = "start_time", nullable = false)
+    private Date startTime;
+
+    @Temporal(TemporalType.TIME)
+    @Column(name = "end_time", nullable = false)
+    private Date endTime;
+
+    @Column(name = "is_available", nullable = false)
+    private Boolean isAvailable = true;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+}
